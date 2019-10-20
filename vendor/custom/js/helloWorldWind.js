0734d537c4085d59e0f8c86d0ad058b6a1bf9281
luisvega1 <luis.vegal@hotmail.com>
2019-10-20 04:34:41 -0600

    :alien: consumo de api

--- vendor/custom/js/helloWorldWind.js
+++ vendor/custom/js/helloWorldWind.js
@@ -1,29 +1,35 @@
-//vincular worldwind con canvas en html
+// Create a WorldWindow for the canvas.
 var wwd = new WorldWind.WorldWindow("canvasOne");
-//mapa alta definición
-//wwd.addLayer(new WorldWind.AtmosphereLayer());
-wwd.addLayer(new WorldWind.BingAerialWithLabelsLayer());
-//mostrar coordenadas 
+
+function evento($event){
+    console.log("loco", $event);
+    var layerName = $event;
+    var serviceAddress = "https://neo.sci.gsfc.nasa.gov/wms/wms?SERVICE=WMS&REQUEST=GetCapabilities&VERSION=1.3.0";
+
+    var createLayer = function (xmlDom) {
+        console.log("init?");
+        var wms = new WorldWind.WmsCapabilities(xmlDom);
+        var wmsLayerCapabilities = wms.getNamedLayer(layerName);
+        var wmsConfig = WorldWind.WmsLayer.formLayerConfiguration(wmsLayerCapabilities);
+        var wmsLayer = new WorldWind.WmsLayer(wmsConfig);
+        wwd.addLayer(wmsLayer);
+    };
+
+    $.get(serviceAddress).done(createLayer).fail(logError);
+}
+
+wwd.addLayer(new WorldWind.BMNGOneImageLayer());
+wwd.addLayer(new WorldWind.BMNGLandsatLayer());
+
+wwd.addLayer(new WorldWind.CompassLayer());
 wwd.addLayer(new WorldWind.CoordinatesDisplayLayer(wwd));
-//mostrar controles
 wwd.addLayer(new WorldWind.ViewControlsLayer(wwd));
 
-agregarAtmosfera = () =>{
-    wwd.addLayer(new WorldWind.AtmosphereLayer());
-}
-//calarle el map
-// var serviceAddress = "https://neo.sci.gsfc.nasa.gov/wms/wms?SERVICE=WMS&REQUEST=GetCapabilities&VERSION=1.3.0";
-// var layerName = "MOD_LSTD_CLIM_M";
-// var createLayer = function (xmlDom) {
-//     var wms = new WorldWind.WmsCapabilities(xmlDom);
-//     var wmsLayerCapabilities = wms.getNamedLayer(layerName);
-//     var wmsConfig = WorldWind.WmsLayer.formLayerConfiguration(wmsLayerCapabilities);
-//     var wmsLayer = new WorldWind.WmsLayer(wmsConfig);
-//     wwd.addLayer(wmsLayer);
-// };
-// var logError = function (jqXhr, text, exception) {
-//     console.log("There was a failure retrieving the capabilities document: " +
-//         text +
-//     " exception: " + exception);
-// };
-// $.get(serviceAddress).done(createLayer).fail(logError);
+// Add WMS imagery
+
+
+var logError = function (jqXhr, text, exception) {
+    console.log("There was a failure retrieving the capabilities document: " +
+        text +
+    " exception: " + exception);
+};
